6fbab002488499b064f51987c1519b4154ad56a5
Campbell Brown <campbellbrown093@gmail.com>
2020-11-10 14:54:57 +1300

    Introduced sprite class

---
+++ DungeonCrawler/DungeonCrawler/Sprite.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler
+{
+    public class Sprite
+    {
+        public Texture2D texture { get; set; }
+
+        public Sprite(Texture2D texture)
+        {
+            this.texture = texture;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+            spriteBatch.Draw(texture, position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+        }
+    }
+}
